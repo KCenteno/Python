05758d5b3e4d47f109561dc307152281aac012cc
Kevin Centeno <KevinCenteno_90@yahoo.com>
2022-02-07 11:28:15 -0500

    first commit

---
+++ Morning Algos/algo1.js
@@ -0,0 +1,34 @@
+//    reversestring
+
+//    "Hello"-> "olleH"
+//    "Geralt" -> "tlareG"
+
+function reversestring(str){
+    var reverse = "";
+    for(var i = str.length - 1; i >= 0; i--){
+        reverse += str[i];
+    }
+    return reverse
+}
+
+console.log(reversestring("creature"))
+
+//    acronym
+
+//    "Welcome To Python" -> "WTP"
+// #  "Hockey Is Expensive" -> "HIE"
+
+function acronym(str) {
+    var acro = ""
+    for(var i = 0; i < str.length; i++){
+        if(i == 0){
+            acro += str[i];
+        }
+        else if(str[i] == " "){
+            acro += str[i+1];
+        }
+    }
+    return acro
+}
+
+console.log(acronym("there's no free lunch - gotta pay yer way"));
