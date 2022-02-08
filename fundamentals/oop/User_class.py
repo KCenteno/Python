d5e75b87ffd5fb1d0a3a7e022b2937c976512151
Kevin Centeno <KevinCenteno_90@yahoo.com>
2022-02-07 21:09:57 -0500

    commit

--- fundamentals/oop/User_class.py
+++ fundamentals/oop/User_class.py
@@ -1,7 +1,6 @@
 class User:
-    def __init__(self, first_name, last_name, email, balance=5000):
+    def __init__(self, first_name, email, balance=5000):
         self.first_name = first_name
-        self.last_name = last_name
         self.email = email
         self.balance = balance
     def make_withdrawal(self, withdraw):
@@ -17,17 +16,17 @@ class User:
     def tranfer_money(self, user, amount):
         self.balance -= amount
         user.balance += amount
-        print(f'User:{self.first_name}, Balance:{self.balance}')
-        print(f'User:{user.first_name}, Balance:{user.balance}')
+        print(f'User:{self.first_name}, New Balance:{self.balance}')
+        print(f'User:{user.first_name}, New Balance:{user.balance}')
         return self
+    def tostring(self):
+        print(f'User:{self.first_name}, New Balance:{self.balance}, email:{self.email}')
 
 
 
-
-
-user1 = User("Kevin", "Centeno", "winner@gmail.com")
-user2 = User("Andrew", "Centeno", "winner@gmail.com")
-user3 = User("Bianca", "Centeno", "winner@gmail.com")
+user1 = User("Kevin Centeno", "winner@gmail.com")
+user2 = User("Andrew Centeno", "winner@gmail.com")
+user3 = User("Bianca Centeno", "winner@gmail.com")
 
 user1.make_deposit(600).make_deposit(600).make_deposit(600).make_withdrawal(1600)
 user2.make_deposit(300).make_deposit(300).make_withdrawal(1000).make_withdrawal(1000)
@@ -37,3 +36,6 @@ user1.check_balance()
 user2.check_balance()
 user3.check_balance()
 user1.tranfer_money(user3, 10000)
+user1.tostring()
+user2.tostring()
+user3.tostring()
