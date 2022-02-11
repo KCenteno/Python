bf89105ec8d55cc1f684faf96011ec39e9269428
Kevin Centeno <KevinCenteno_90@yahoo.com>
2022-02-11 11:00:07 -0500

    algo5

---
+++ Morning Algos/algo5.js
@@ -0,0 +1,38 @@
+//   convertCoinChange(money)
+
+// 50 
+
+// logic
+// logic
+// logic
+// logic
+
+// 2 quarters
+
+
+function convertCoinChange(money){
+    var quarters = Math.floor(money/25);
+    money-=25*quarters;
+    var dimes = Math.floor(money/10);
+    money-=10*dimes;
+    var nickels = Math.floor(money/5);
+    money-=5*nickels;
+    pennies = money;
+    return [quarters,dimes,nickels,pennies];
+}
+console.log(convertCoinChange(197))
+
+function convertCoinChange2(money){
+    if(money%1 != 0){
+        money*=100;
+    }
+    var moneys = [25,10,5,1];
+    var out = [];
+    for(var i = 0; i < moneys.length; i++){
+        out.push(Math.floor(money/moneys[i]));
+        money = money%moneys[i];
+    }
+    return out;
+}
+
+console.log(convertCoinChange2(1.80))
